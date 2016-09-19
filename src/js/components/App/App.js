5ba20a8b570013c16212a922ebbdd2d5e56b7572
Frode Sundby <frodesun@gmail.com>
2016-09-19 21:47:37 +0200

    Legger til constants, actions og en enkel counter i app

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -1,15 +1,26 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
+import {increment, decrement} from '../../actions/counterActions'
 
 class App extends React.Component {
     constructor(props){
         super(props)
     }
+    handleClick(type){
+        if (type === "increment") {
+            this.props.dispatch(increment())
+        } else {
+            this.props.dispatch(decrement())
+        }
+    }
     render(){
-        console.log("in app.js", this.props.counter)
+        const { counter, dispatch }  = this.props
         return (
             <div>
                 <h1>Hallo igjen</h1>
+                <div>{counter}</div>
+                <button type="button" onClick={this.handleClick.bind(this, "increment")}>increment</button>
+                <button type="button" onClick={this.handleClick.bind(this, "decrement")}>decrement</button>
 
             </div>
         )
